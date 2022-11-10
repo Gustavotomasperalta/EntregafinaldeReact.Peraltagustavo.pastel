914072cdf5060b49c2812008ebf33126c8894437
Gustavotomasperalta <gustavo.tomas.peralta2018@gmail.com>
2022-11-10 19:43:16 -0300

    arreglando lo de firebase

--- src/services/firestore.js
+++ src/services/firestore.js
@@ -1,31 +1,18 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import {
-  collection,
-  getFirestore,
-  getDocs,
-  getDoc,
-  doc,
-  query,
-  where,
-  addDoc,
-} from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { collection, getDocs, getDoc, query, where, addDoc, doc} from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyAJteutJIO5fTR9NFWa_CtOpR7_0t0opLU",
-  authDomain: "react-37555.firebaseapp.com",
-  projectId: "react-37555",
-  storageBucket: "react-37555.appspot.com",
-  messagingSenderId: "745774296744",
-  appId: "1:745774296744:web:f2d8e41b1de53f74f95c9d",
+  apiKey: "AIzaSyCzWk8wzANiCjBGlx4vI7eNQwQE8Y0XTIM",
+  authDomain: "react-peralta.firebaseapp.com",
+  projectId: "react-peralta",
+  storageBucket: "react-peralta.appspot.com",
+  messagingSenderId: "155060447910",
+  appId: "1:155060447910:web:84e37fe8d834badf55a267"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const firestore = getFirestore(app);
+export const firestore = getFirestore(app);
 
 export async function getItems() {
   const miColleccion = collection(firestore, "tortas");
